1fb499fedf166c460ec43ec263a7f47113190bab
Daniel Pomfret <daniel.pomfret@dwp.gsi.gov.uk>
2019-03-07 11:46:26 +0000

    First commit!

---
+++ build/precache-manifest.c2775abe898fc23d6c0795ed079fa1fd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f46b2d112159588c8fa0",
+    "url": "/static/js/main.5837045e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "36284f00c48da399a1bf",
+    "url": "/static/js/2.4ba58cf5.chunk.js"
+  },
+  {
+    "revision": "d799b62ef5cf003c376d95e3a59b51a3",
+    "url": "/index.html"
+  }
+];
---
+++ src/Navigator.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home';
+import PeopleIcon from '@material-ui/icons/People';
+import LayersIcon from '@material-ui/icons/Layers';
+import EventIcon from '@material-ui/icons/Event';
+import GamepadIcon from '@material-ui/icons/Gamepad';
+import PoolIcon from '@material-ui/icons/Pool';
+
+const categories = [
+  {
+    id: 'Group',
+    children: [
+      { id: 'Sections', icon: <LayersIcon />, active: true },
+      { id: 'Members', icon: <PeopleIcon /> },
+    ],
+  },
+  {
+    id: 'Programme',
+    children: [
+      { id: 'Meetings', icon: <GamepadIcon /> },
+      { id: 'Events', icon: <EventIcon /> },
+      { id: 'Activities', icon: <PoolIcon /> },
+    ],
+  },
+];
+
+const styles = theme => ({
+  categoryHeader: {
+    paddingTop: 16,
+    paddingBottom: 16,
+  },
+  categoryHeaderPrimary: {
+    color: theme.palette.common.white,
+  },
+  item: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    color: 'rgba(255, 255, 255, 0.7)',
+  },
+  itemCategory: {
+    backgroundColor: '#232f3e',
+    boxShadow: '0 -1px 0 #404854 inset',
+    paddingTop: 16,
+    paddingBottom: 16,
+  },
+  firebase: {
+    fontSize: 24,
+    fontFamily: theme.typography.fontFamily,
+    color: theme.palette.common.white,
+  },
+  itemActionable: {
+    '&:hover': {
+      backgroundColor: 'rgba(255, 255, 255, 0.08)',
+    },
+  },
+  itemActiveItem: {
+    color: '#ffe627',
+  },
+  itemPrimary: {
+    color: 'inherit',
+    fontSize: theme.typography.fontSize,
+    '&$textDense': {
+      fontSize: theme.typography.fontSize,
+    },
+  },
+  textDense: {},
+  divider: {
+    marginTop: theme.spacing.unit * 2,
+  },
+});
+
+function Navigator(props) {
+  const { classes, ...other } = props;
+
+  return (
+    <Drawer variant="permanent" {...other}>
+      <List disablePadding>
+        <ListItem className={classNames(classes.firebase, classes.item, classes.itemCategory)}>
+          Coly
+        </ListItem>
+        <ListItem className={classNames(classes.item, classes.itemCategory)}>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText
+            classes={{
+              primary: classes.itemPrimary,
+            }}
+          >
+            1st Haughton Green
+          </ListItemText>
+        </ListItem>
+        {categories.map(({ id, children }) => (
+          <React.Fragment key={id}>
+            <ListItem className={classes.categoryHeader}>
+              <ListItemText
+                classes={{
+                  primary: classes.categoryHeaderPrimary,
+                }}
+              >
+                {id}
+              </ListItemText>
+            </ListItem>
+            {children.map(({ id: childId, icon, active }) => (
+              <ListItem
+                button
+                dense
+                key={childId}
+                className={classNames(
+                  classes.item,
+                  classes.itemActionable,
+                  active && classes.itemActiveItem,
+                )}
+              >
+                <ListItemIcon>{icon}</ListItemIcon>
+                <ListItemText
+                  classes={{
+                    primary: classes.itemPrimary,
+                    textDense: classes.textDense,
+                  }}
+                >
+                  {childId}
+                </ListItemText>
+              </ListItem>
+            ))}
+            <Divider className={classes.divider} />
+          </React.Fragment>
+        ))}
+      </List>
+    </Drawer>
+  );
+}
+
+Navigator.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Navigator);
